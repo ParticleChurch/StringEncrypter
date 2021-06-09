e79148e6758277133e1466311ddd2277b456e4ff
ParticleChurch <70173558+ParticleChurch@users.noreply.github.com>
2021-06-09 14:36:09 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -9,96 +9,82 @@ inline uint32_t random()
 	return rng() % (uint32_t)-1;
 }
 
-#define MINSTRINGSIZE (sizeof("!XOR!\0") - 1)
-void encryptStrings(char* f, size_t size)
+size_t encryptStrings(char* data, size_t size)
 {
-	for (size_t filePosition = 0, nBytesRemaining = size; nBytesRemaining >= MINSTRINGSIZE; filePosition++, nBytesRemaining--)
+	size_t count = 0;
+	for (size_t fpos = 0; fpos < size; fpos++)
 	{
-		char* tag = f + filePosition;
-		if (strncmp(tag, "!XOR", 4))
+		if (strncmp(data + fpos, "!XOR!", 5) != 0)
 			continue;
-		
-		size_t stringSize = 0;
-		char* string = nullptr;
-		if (tag[4] == '!')
-		{
-			if (nBytesRemaining <= 5) return;
-			stringSize = strnlen_s(tag + 5, nBytesRemaining - 5) + 1; // +=1 for null terminator
-			if (stringSize > nBytesRemaining) return;
-			string = tag + 5;
-		}
-		else
-		{
-			for (size_t i = 4; i < nBytesRemaining; i++)
-			{
-				if ('0' <= tag[i] && tag[i] <= '9')
-				{
-					stringSize = stringSize * 10 + (tag[i] - '0');
-				}
-				else if (tag[i] == '!')
-				{
-					string = tag + i + 1;
-					break;
-				}
-				else
-				{
-					std::cout << "Found near match at file position " << filePosition << std::endl;
-					break;
-				}
-			}
-			if (!string)
-				continue;
-		}
 
-
+		// found a string!
+		char* str = data + fpos;
 		uint32_t key = random();
-		std::cout << key << "/" << stringSize << " : " << std::string(string, std::min(stringSize, (size_t)16))<< std::endl;
+		const char* keys = (char*)(&key);
 
-		tag[0] = '^';
-		*(uint32_t*)(tag + 1) = key;
+		std::cout << "Encrypting (" << key << "): " << str << std::endl;
+		count++;
 
-		size_t offset = 0;
-		for (; stringSize >= 4 && offset <= stringSize - 4; offset += 4)
-			*(uint32_t*)(string + offset) ^= key;
-		for (; offset < stringSize; offset++)
-			string[offset] ^= key & 0xff;
+		*str = '^';
+		*(uint32_t*)(str + 1) = key;
+
+		size_t o = 0;
+		while (true)
+		{
+			bool stop = str[5 + o] == '\0';
+			str[5 + o] ^= keys[o % 4];
+
+			if (stop)
+				break;
+			else
+				o++;
+		}
 	}
+	return count;
 }
 
 int main()
 {
 	srand((unsigned int)(time(0)));
-	std::fstream ifile;
-	std::fstream ofile;
-	ifile.open("E:\\GitHub\\internalV1\\Release\\CSGOCollabV1.dll", std::fstream::in | std::fstream::binary);
-	ofile.open("E:\\GitHub\\internalV1\\Release\\encrypted.dll", std::fstream::out | std::fstream::binary);
 
-	if (!ifile.is_open() || !ofile.is_open())
+	// read input file
+	std::fstream ifile;
+	ifile.open("E:\\GitHub\\internalV1\\Release\\CSGOCollabV1.dll", std::fstream::in | std::fstream::binary);
+	if (!ifile.is_open())
 	{
-		std::cout << "Failed to open file(s)." << std::endl;
+		std::cout << "Failed to open input file." << std::endl;
 		return 1;
 	}
-
 	ifile.seekg(0, std::fstream::end);
 	size_t size = (size_t)(ifile.tellg());
 	ifile.seekg(0);
-
 	char* f = new char[size];
 	if (!ifile.read(f, size))
 	{
 		std::cout << "Failed to read file." << std::endl;
 		return 1;
 	}
+	ifile.close();
 
-	encryptStrings(f, size);
+	// create output file
+	std::fstream ofile;
+	ofile.open("E:\\GitHub\\internalV1\\Release\\encrypted.dll", std::fstream::out | std::fstream::binary);
+	if (!ofile.is_open())
+	{
+		std::cout << "Failed to open output file." << std::endl;
+		return 1;
+	}
 
-	if (!ofile.write(f, size))
+	char* out = new char[size];
+	memcpy(out, f, size);
+	size_t count = encryptStrings(out, size);
+	std::cout << "Encrypted a total of " << count << " strings." << std::endl;
+
+	if (!ofile.write(out, size))
 	{
 		std::cout << "Failed to write output." << std::endl;
 		return 1;
 	}
-
-	ifile.close();
 	ofile.close();
 
 	return 0;
